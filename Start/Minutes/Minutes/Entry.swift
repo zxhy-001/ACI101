55620a7dbe3c7a8fb3201aed85e4f341e4d71516
zxhy-001 <zxhy_001@outlook.com>
2021-10-31 22:54:59 +0800

    Hello

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Entry: Codable
 {
-    var title:   String = ""
+    var title:   String = "Hello"
     var content: String = ""
     
     var id: String = UUID().uuidString
